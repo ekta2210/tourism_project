36d6986a8da55da8357b4644d0b9715aa7dcd3b0
Ekta soni <165471025+ekta2210@users.noreply.github.com>
2026-07-16 16:29:28 +0530

    Update hosting.py

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -1,8 +1,18 @@
 from huggingface_hub import HfApi
+from huggingface_hub import create_repo
 import os
 
 # Initialize Hugging Face API
 api = HfApi(token=os.getenv("HF_TOKEN"))
+
+create_repo(
+    repo_id="ektasoni2210/visit-with-us-prediction",
+    repo_type="space",
+    space_sdk="docker",  # or "streamlit"/"gradio" depending on what's inside your `deployment` folder
+    exist_ok=True,
+    private=False,
+    token=os.getenv("HF_TOKEN")
+)
 
 # Upload the deployment folder to Hugging Face Space
 api.upload_folder(
