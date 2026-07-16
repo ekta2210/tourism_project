2ce3e8f457e2ff6dd4d039a0f1d3380dc7517c27
Ekta soni <165471025+ekta2210@users.noreply.github.com>
2026-07-16 16:18:09 +0530

    Update hosting.py

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -1,4 +1,3 @@
-
 from huggingface_hub import HfApi
 import os
 
@@ -8,7 +7,7 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 # Upload the deployment folder to Hugging Face Space
 api.upload_folder(
     folder_path="tourism_project/deployment",
-    repo_id="<your_huggingface_username>/visit-with-us-prediction",
+    repo_id="ektasoni2210/visit-with-us-prediction",
     repo_type="space",
     path_in_repo=""
 )
